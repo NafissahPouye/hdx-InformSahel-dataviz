84d57a92350bfbc320481c3f963f80a902500e6f
Nafissah Pouye <Nafissah Pouye>
2017-09-20 16:23:51 +0000

    remove the undefined

--- js/site.js
+++ js/site.js
@@ -14,14 +14,14 @@ dc.dataCount('#count-info')
   .dimension(cf)
   .group(all);
 
-         Imap.width(600)
-             .height(450)
+         Imap.width(400)
+             .height(400)
              .dimension(mapDimension)
              .group(mapGroup)
              .center([0,0])
              .zoom(0)
              .geojson(vargeodata)
-             .colors(['#DDDDDD',"#ffe6e3","#e85945", "#911200", '#730d00'])
+             .colors(['#DDDDDD','#ffe6e3','#e85945', '#911200', '#730d00'])
              .colorDomain([0,4])
              .colorAccessor(function (d){
               var c = 0
@@ -50,45 +50,8 @@ dc.dataCount('#count-info')
                 'fillOpacity': 0.1,
                 'weight': 1
             });
+
 //dataTable 1
-dataTab1
-        .size(400)
-        .dimension(mapDimension)
-        .group(function (d) {
-            return d.mapGroup;
-        })
-        .columns([
-                    function (d) {
-                return d.COUNTRY;
-                },
-
-                    function (d) {
-                return d.ADMIN1;
-                },
-                       
-                 function (d) {
-                return d.Food_Insecurity_Probability;
-                },
-                 function (d) {
-                return d.Physical_exposure_to_flood;
-                },
-                 function (d) {
-                return d.Land_Degradation;
-                },
-                 function (d) {
-                return d.Droughts_impact;
-                },
-                function (d) {
-                return d.Hazard_2017;
-                }
-
-                                   ])
-        /*.sortBy(function (d) {
-            return [d.COUNTRY, d.ADMIN1, d.RISK, d.Food_Insecurity_Probability, d.Physical_exposure_to_flood, d.Land_Degradation, d.Droughts_impact, d.Hazard_2017];
-            // return d[config.whoFieldName];
-        });*/
-//dataTable 2
-
 dataTab2
         .size(400)
         .dimension(mapDimension)
@@ -105,32 +68,231 @@ dataTab2
                 },
                         
                   function (d) {
-                return d.FoodInsecurityProbability;
+                return d.Food_Insecurity_Probability;
                 },
                  function (d) {
-                return d.DroughtsImpact;
+                return d.Physical_exposure_to_flood;
                 },
                  function (d) {
-                return d.LandDegradation;
+                return d.Land_Degradation;
                 },
                  function (d) {
-                return d.DroughtsImpact;
+                return d.Droughts_probability_and_historical_impact;
                 },
                 function (d) {
-                return d.Hazard_2016;
+                return d.Natural;
+                },
+                function (d) {
+                return d.Political_violence;
+                },
+                function (d) {
+                return d.Conflict_probability;
+                },
+                function (d) {
+                return d.Human;
+                },
+                function (d) {
+                return d.HAZARD;
+                },
+                function (d) {
+                return d.Development_Deprivation;
+                },
+                function (d) {
+                return d.Inequality;
+                },
+                function (d) {
+                return d.Aid_Dependency;
+                },
+                function (d) {
+                return d.Socio_Economic_Vulnerability;
+                },
+                function (d) {
+                return d.Uprooted_people;
+                },
+                function (d) {
+                return d.Health_Conditions;
+                },
+                function (d) {
+                return d.Children_U5;
+                },
+                function (d) {
+                return d.Malnutrition;
+                },
+                function (d) {
+                return d.Malnutrition;
+                },
+                function (d) {
+                return d.Recent_Shocks;
+                },
+                function (d) {
+                return d.Food_Security;
+                },
+                function (d) {
+                return d.Other_Vulnerable_Groups;
+                },
+                function (d) {
+                return d.Vulnerable_Groups;
+                },
+                function (d) {
+                return d.VULNERABILITY;
+                },
+                function (d) {
+                return d.DRR;
+                },
+                function (d) {
+                return d.Governance;
+                },
+                function (d) {
+                return d.Institutional;
+                },
+                function (d) {
+                return d.Communication;
+                },
+                function (d) {
+                return d.Physical_infrastructure;
+                },
+                function (d) {
+                return d.Access_to_health_care;
+                },
+                function (d) {
+                return d.Infrastructure;
+                },
+                function (d) {
+                return d.LACK_OF_COPING_CAPACITY;
                 }
 
-                                   ])
-        .sortBy(function (d) {
-            return [d.COUNTRY, d.ADMIN1, d.FoodInsecurityProbability, d.DroughtsImpact, d.LandDegradation, d.Hazard_2016];
-            // return d[config.whoFieldName];
-        });
+                                               ])
+
+         dataTab1.renderlet(function (chart) {
+                    chart.selectAll("g.x text")
+                        .style("text-anchor", "end")
+                        //.attr('dx', '0')
+                      
+                        .attr('transform', "rotate(-85)");
+                })
+
+
+//dataTable 2
+dataTab1
+        .size(400)
+        .dimension(mapDimension)
+        .group(function (d) {
+            return d.mapGroup;
+        })
+        //.title(2017)
+        .columns([
+                    function (d) {
+                return d.COUNTRY;
+                },
+
+                    function (d) {
+                return d.ADMIN1;
+                },
+                        
+                  function (d) {
+                return d.Food_Insecurity_Probability_2016;
+                },
+                 function (d) {
+                return d.Physical_exposure_to_flood_2016;
+                },
+                 function (d) {
+                return d.Land_Degradation_2016;
+                },
+                 function (d) {
+                return d.Droughts_probability_and_historical_impact_2016;
+                },
+                function (d) {
+                return d.Natural_2016;
+                },
+                function (d) {
+                return d.Political_violence_2016;
+                },
+                function (d) {
+                return d.Conflict_probability_2016;
+                },
+                function (d) {
+                return d.Human_2016;
+                },
+                function (d) {
+                return d.HAZARD_2016;
+                },
+                function (d) {
+                return d.Development_Deprivation_2016;
+                },
+                function (d) {
+                return d.Inequality_2016;
+                },
+                function (d) {
+                return d.Aid_Dependency_2016;
+                },
+                function (d) {
+                return d.Socio_Economic_Vulnerability_2016;
+                },
+                function (d) {
+                return d.Uprooted_people_2016;
+                },
+                function (d) {
+                return d.Health_Conditions_2016;
+                },
+                function (d) {
+                return d.Children_U5_2016;
+                },
+                function (d) {
+                return d.Malnutrition_2016;
+                },
+                function (d) {
+                return d.Malnutrition_2016;
+                },
+                function (d) {
+                return d.Recent_Shocks_2016;
+                },
+                function (d) {
+                return d.Food_Security_2016;
+                },
+                function (d) {
+                return d.Other_Vulnerable_Groups_2016;
+                },
+                function (d) {
+                return d.Vulnerable_Groups_2016;
+                },
+                function (d) {
+                return d.VULNERABILITY_2016;
+                },
+                function (d) {
+                return d.DRR_2016;
+                },
+                function (d) {
+                return d.Governance_2016;
+                },
+                function (d) {
+                return d.Institutional_2016;
+                },
+                function (d) {
+                return d.Communication_2016;
+                },
+                function (d) {
+                return d.Physical_infrastructure_2016;
+                },
+                function (d) {
+                return d.Access_to_health_care_2016;
+                },
+                function (d) {
+                return d.Infrastructure_2016;
+                },
+                function (d) {
+                return d.LACK_OF_COPING_CAPACITY_2016;
+                }
+])
+      
+      .sortBy(function (d) {                                         
+      return [d.COUNTRY, d.ADMIN1, d.Food_Insecurity_Probability_2016]; 
+       }); 
+
       dc.renderAll();
 
       var map = Imap.map();
 
       zoomToGeom(vargeodata);
-
       function zoomToGeom(geodata){
         var bounds = d3.geo.bounds(geodata) ;
         map.fitBounds([[bounds[0][1],bounds[0][0]],[bounds[1][1],bounds[1][0]]]);
@@ -147,7 +309,8 @@ dataTab2
 
 var dataCall = $.ajax({
     type: 'GET',
-    url: 'data/InformData.json',
+    url: 'data/InformData2.json',
+    //url: 'data/InformData1.json',
     dataType: 'json',
 });
 
